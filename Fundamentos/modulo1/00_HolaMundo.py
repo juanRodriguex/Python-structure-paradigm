cffb88bbd6e190c631e6c7bc7b1c380688bc8413
juanRodriguex <juan.rodriguez67049@ucaldas.edu.co>
2026-09-04 22:46:37 -0500

    varibles

---
+++ Fundamentos/modulo1/00_HolaMundo.py
@@ -0,0 +1,10 @@
+#Hola mundo
+
+"""
+Este comentario es largo
+
+"""
+
+print("Hola, mundo!")
+print(type(2))
+print(type(2+9j))
---
+++ Fundamentos/modulo1/01_variables.py
@@ -0,0 +1,13 @@
+#variables
+my_bool_variable = True
+my_int_variable = 10
+print(my_int_variable)
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+
+#Concatenación
+
+print(my_int_to_str_variable, my_bool_variable)
+#Funciones del Sistema
+
+print(len(my_int_to_str_variable))
